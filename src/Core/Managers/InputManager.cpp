0154224600b16601b949686b844775c23e9398bb
Leon Brands (Windows Desktop) <lajbrands@outlook.com>
2021-02-08 00:41:31 +0100

    Namespace formatting

--- src/Core/Managers/InputManager.cpp
+++ src/Core/Managers/InputManager.cpp
@@ -7,30 +7,24 @@
 #include <GLFW/glfw3.h>
 #include "Core/BindingData.h"
 
-namespace Tristeon
+namespace Tristeon::Core::Managers
 {
-	namespace Core
+	InputManager::InputManager()
 	{
-		namespace Managers
-		{
-			InputManager::InputManager()
-			{
-				GLFWwindow* window = binding_data.window;
+		GLFWwindow* window = binding_data.window;
 				
-				MessageBus::subscribeToMessage(MT_AFTERFRAME, [&](Message msg) { resetInput(); });
-				Misc::Mouse::window = window;
-				glfwSetKeyCallback(window, [](GLFWwindow* window, int key, int scancode, int action, int mods) { Misc::Keyboard::keyCallback(key, scancode, action, mods); });
-				glfwSetMouseButtonCallback(window, [](GLFWwindow* window, int button, int action, int mods) { Misc::Mouse::buttonCallback(button, action, mods); });
-				glfwSetCursorPosCallback(window, [](GLFWwindow* window, double x, double y) { Misc::Mouse::positionCallback(x, y); });
-				glfwSetCursorEnterCallback(window, [](GLFWwindow* window, int action) { Misc::Mouse::windowEnterCallback(static_cast<bool>(action)); });
-				glfwSetScrollCallback(window, [](GLFWwindow* window, double x, double y) { Misc::Mouse::scrollCallback(x, y); });
-			}
+		MessageBus::subscribeToMessage(MT_AFTERFRAME, [&](Message msg) { resetInput(); });
+		Misc::Mouse::window = window;
+		glfwSetKeyCallback(window, [](GLFWwindow* window, int key, int scancode, int action, int mods) { Misc::Keyboard::keyCallback(key, scancode, action, mods); });
+		glfwSetMouseButtonCallback(window, [](GLFWwindow* window, int button, int action, int mods) { Misc::Mouse::buttonCallback(button, action, mods); });
+		glfwSetCursorPosCallback(window, [](GLFWwindow* window, double x, double y) { Misc::Mouse::positionCallback(x, y); });
+		glfwSetCursorEnterCallback(window, [](GLFWwindow* window, int action) { Misc::Mouse::windowEnterCallback(static_cast<bool>(action)); });
+		glfwSetScrollCallback(window, [](GLFWwindow* window, double x, double y) { Misc::Mouse::scrollCallback(x, y); });
+	}
 
-			void InputManager::resetInput() const
-			{
-				Misc::Keyboard::reset();
-				Misc::Mouse::reset();
-			}
-		}
+	void InputManager::resetInput()
+	{
+		Misc::Keyboard::reset();
+		Misc::Mouse::reset();
 	}
 }
--- src/Core/Managers/InputManager.h
+++ src/Core/Managers/InputManager.h
@@ -1,33 +1,23 @@
 ﻿#pragma once
-struct GLFWwindow;
-
-namespace Tristeon
+namespace Tristeon::Core::Managers
 {
-	namespace Core
+	/**
+	 * InputManager manages all the input calls coming from the window context and caches them for usage as Mouse/Keyboard/Joystick data
+	 *
+	 * This class is not intended to be accessed or used by users.
+	 */
+	class InputManager final
 	{
-		class Engine;
+	public:
+		/**
+		 * \param window The GLFW window, used to subscribe to callbacks
+		 */
+		explicit InputManager();
 
-		namespace Managers
-		{
-			/**
-			 * InputManager manages all the input calls coming from the window context and caches them for usage as Mouse/Keyboard/Joystick data
-			 *
-			 * This class is not intended to be accessed or used by users.
-			 */
-			class InputManager final
-			{
-			public:
-				/**
-				 * \param window The GLFW window, used to subscribe to callbacks 
-				 */
-				explicit InputManager();
-
-			private:
-				/**
-				 * Resets keydown and keyup, called every frame
-				 */
-				void resetInput() const;
-			};
-		}
-	}
+	private:
+		/**
+		 * Resets keydown and keyup, called every frame
+		 */
+		static void resetInput();
+	};
 }
